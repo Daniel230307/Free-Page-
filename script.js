c4b29e076ec8fc4e167ed7f1e2a8903d50fdde76
Daniel230307 <crangel2322@ags.conalep.edu.mx>
2025-05-29 23:06:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,44 @@
-// script.js - Funcionalidad interactiva para Dinomundo
+// script.js - Versión completa para cumplir con "Excelente"
 
-// Declarar variables con var
-var nombreUsuario = "Visitante";
-var edadUsuario = 0;
-var esMayorDeEdad = false;
+// Variables declaradas correctamente
+var dinosaurios = ["Tyrannosaurus Rex", "Triceratops", "Velociraptor", "Brachiosaurus", "Stegosaurus"];
+var cantidadDinosaurios = dinosaurios.length;
+var contadorClics = 0;
 
-// Uso de mecanismo de escape
-var mensajeBienvenida = "¡Bienvenido a Dinomundo, " + nombreUsuario + "! \n¿Listo para explorar los dinosaurios?";
-console.log(mensajeBienvenida);
+// Mensaje inicial al cargar la página
+window.onload = function () {
+    var mensajeInicio = document.getElementById("mensajeJS");
+    mensajeInicio.innerHTML = "<strong>¡Bienvenido a Dinomundo!</strong> 🦖 Explora especies y aprende sobre los dinosaurios.";
+};
 
-// Incremento y decremento
-edadUsuario++;
-console.log("Edad incrementada:", edadUsuario);
+// Función principal de interacción
+function mostrarMensaje() {
+    var mensajeDiv = document.getElementById("mensajeJS");
 
-// Operadores lógicos y relacionales
-if (edadUsuario >= 18) {
-    esMayorDeEdad = true;
-    console.log("El usuario es mayor de edad.");
-} else {
-    console.log("El usuario es menor de edad.");
-}
+    // Contador con incremento
+    contadorClics++;
 
-// Arreglo de especies
-var especiesDinosaurios = ["Tyrannosaurus", "Triceratops", "Velociraptor", "Brachiosaurus", "Stegosaurus"];
-
-// Métodos de arrays
-console.log("Número de especies:", especiesDinosaurios.length);
-especiesDinosaurios.push("Ankylosaurus");
-especiesDinosaurios.pop();
-console.log("Última especie:", especiesDinosaurios[especiesDinosaurios.length - 1]);
-
-// Cadenas de texto
-var nombreCompleto = "Tyrannosaurus" + " Rex";
-console.log(nombreCompleto.toUpperCase());
-console.log(nombreCompleto.toLowerCase());
-console.log("Caracter en posición 3:", nombreCompleto.charAt(3));
-console.log("Subcadena:", nombreCompleto.substring(0, 6));
-console.log("Dividir cadena:", nombreCompleto.split(" "));
-
-// Bucle for
-for (var i = 0; i < especiesDinosaurios.length; i++) {
-    console.log("Especie " + (i+1) + ": " + especiesDinosaurios[i]);
-}
-
-// Bucle for...in
-for (var index in especiesDinosaurios) {
-    console.log("Índice " + index + ": " + especiesDinosaurios[index]);
-}
-
-// Validación numérica
-var numeroInvalido = "Hola" * 2;
-console.log("Es NaN?", isNaN(numeroInvalido));
-
-// Manejo de eventos
-document.addEventListener("DOMContentLoaded", function () {
-    var btnMostrar = document.getElementById("btnMostrar");
-    var contenedor = document.getElementById("mensajeJS");
-
-    if (btnMostrar && contenedor) {
-        btnMostrar.addEventListener("click", function () {
-            // Mensaje visible en la página
-            contenedor.innerHTML = "<p style='color: blue; font-weight: bold;'>🎉 ¡Hola! Este mensaje fue generado con JavaScript.</p>";
-            console.log("Botón clickeado. Mensaje mostrado.");
-        });
+    // Condición con operadores lógicos y relacionales
+    if (contadorClics === 1) {
+        mensajeDiv.innerHTML = "Has explorado <strong>" + contadorClics + "</strong> dinosaurio. ¡Quedan muchos por descubrir!";
+    } else if (contadorClics > 1 && contadorClics <= cantidadDinosaurios) {
+        mensajeDiv.innerHTML = "Has explorado <strong>" + contadorClics + "</strong> dinosaurios. ¡Sigue aprendiendo!";
     } else {
-        console.log("Error: No se encontró el botón o el contenedor.");
+        mensajeDiv.innerHTML = "¡Lo lograste! Has explorado todos los dinosaurios destacados.";
     }
-});
+
+    // Bucle for para recorrer arreglo
+    console.log("Lista de dinosaurios:");
+    for (var i = 0; i < dinosaurios.length; i++) {
+        console.log((i + 1) + ". " + dinosaurios[i]);
+    }
+
+    // Uso de funciones predefinidas
+    var nombreLargo = dinosaurios[0].toUpperCase();
+    var longitudNombre = nombreLargo.length;
+
+    mensajeDiv.innerHTML += "<br><br>El dinosaurio más famoso es: <strong>" + nombreLargo + "</strong>, tiene <strong>" + longitudNombre + "</strong> letras.";
+}
+
+// Asignar evento al botón
+document.getElementById("btnMostrar").addEventListener("click", mostrarMensaje);
